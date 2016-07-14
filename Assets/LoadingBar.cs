802e52be751ca918f6b8be80512685c43bb6c6e1
phuongho43 <phuongho43@gmail.com>
2016-07-13 17:36:46 -0700

    Final result from Spring2016

---
+++ Assets/LoadingBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Threading;
+public class LoadingBar : MonoBehaviour {
+
+    public Transform Progress;
+    int current;
+    void setup()
+    {
+        current = 0;
+        Progress.GetComponent<Image>().fillAmount = 0;
+    }
+	// Update is called once per frame
+	void Update () {
+        Progress.GetComponent<Image>().fillAmount = current / 100;
+        current++;
+	}
+}
