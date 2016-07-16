2e436163e67e3a712337aaa44846195b313fda6c
phuongho43 <phuongho43@gmail.com>
2016-07-16 01:07:11 -0700

    Added sqlite db, create c# scripts to insert data, delete data, and output data using debug.log to console (all executed upon clicking play in unity editor)

---
+++ Assets/Scripts/PatientDatabaseHandler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.Collections.Generic;
+
+public class PatientDatabaseHandler : MonoBehaviour {
+
+	private string connectionString;
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello World");
+		connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
+		//InsertPatientData("Charlie", "Mander");
+		//DeletePatient(7);
+		GetPatientData();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void GetPatientData () {
+		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
+			dbConnection.Open();
+			using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
+				string sqlQuery = "SELECT first_name, last_name from patients";
+				dbCmd.CommandText = sqlQuery;
+				using (IDataReader reader = dbCmd.ExecuteReader()) {
+					while (reader.Read()) {
+						string first_name = reader.GetString(0);
+						string last_name = reader.GetString(1);
+						Debug.Log(first_name + " " + last_name);
+					}
+					dbConnection.Close();
+					reader.Close();
+				}
+			}
+		}
+	}
+
+	private void InsertPatientData (string first_name, string last_name) {
+		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
+			dbConnection.Open();
+			using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
+				string sqlQuery = String.Format("INSERT INTO patients(first_name,last_name) VALUES('{0}', '{1}')", first_name, last_name);
+				dbCmd.CommandText = sqlQuery;
+				dbCmd.ExecuteScalar();
+				dbConnection.Close();
+			}
+		}
+	}
+
+	private void DeletePatient (int patient_id) {
+		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
+			dbConnection.Open();
+			using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
+				string sqlQuery = String.Format("DELETE FROM patients WHERE patient_id = {0}", patient_id);
+				dbCmd.CommandText = sqlQuery;
+				dbCmd.ExecuteScalar();
+				dbConnection.Close();
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/PatientInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class PatientInfo {
+	public string First_name {get; set;}
+	public string Last_name {get; set;}
+
+	public PatientInfo(string first_name, string last_name) {
+		this.First_name = first_name;
+		this.Last_name = last_name;
+	}
+}
