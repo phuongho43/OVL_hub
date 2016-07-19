97f933448fedc1a18b54560e45b17929aed5d57f
phuongho43 <phuongho43@gmail.com>
2016-07-19 01:03:23 -0700

    Revert "Created class for patientProfile, coded list element to PatientDatabaseHandler and have it where GetPatientData() clears previous list and adds first_name, last_name from query"
    This reverts commit 71b9a276e3e0081ef4ed7988d65c5359db9eaf09.

--- Assets/Scripts/PatientInfo.cs
+++ Assets/Scripts/PatientInfo.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-class PatientProfile {
+class PatientInfo {
 	public string First_name {get; set;}
 	public string Last_name {get; set;}
 
-	public PatientProfile(string first_name, string last_name) {
+	public PatientInfo(string first_name, string last_name) {
 		this.First_name = first_name;
 		this.Last_name = last_name;
 	}
