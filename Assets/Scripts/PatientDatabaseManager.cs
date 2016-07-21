6f7d3e1f2a58c6cc45f480f15a8910b292c5736d
phuongho43 <phuongho43@gmail.com>
2016-07-21 00:00:58 -0700

    Created SetPatientData class to call GetPatientData and update texts in the patientprofile.
    Still need to be able to pass in latest patient_id from form and
    instantiate medications + conditions list items.

--- Assets/Scripts/PatientDatabaseManager.cs
+++ Assets/Scripts/PatientDatabaseManager.cs
@@ -4,17 +4,26 @@ using System.Collections;
 using System.Data;
 using Mono.Data.Sqlite;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 
 public class PatientDatabaseManager : MonoBehaviour {
 
 	private string connectionString;
-	public GameObject full_name_textbox;
-	//private List<PatientProfile> patientProfile = new List<PatientProfile>();
+	public Text id_textbox;
+	public Text fullname_textbox;
+	public Text gender_textbox;
+	public Text birthdate_textbox;
+	public Text weight_textbox;
+	public Text height_textbox;
+	public Text phone_textbox;
+	public Text appointment_textbox;
+	public Text latest_visit_date_textbox;
+	public Text latest_visit_doc_textbox;
 
 	// Use this for initialization
 	void Start () {
-		//connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
+		//SetPatientData (2);
 		//InsertPatientData("Charlie", "Mander");
 		//DeletePatient(7);
 		//GetPatientData(2);
@@ -25,28 +34,42 @@ public class PatientDatabaseManager : MonoBehaviour {
 
 	}
 
-	public void GetPatientData (int patient_id) {
-		//patientProfile.Clear();
+	private List<string> GetPatientData (int patient_id) {
+		List<string> patient_data = new List<string>();
 		connectionString = "URI=file:" + Application.dataPath + "/hubDB.db";
 		using (IDbConnection dbConnection = new SqliteConnection(connectionString)) {
 			dbConnection.Open();
 			using (IDbCommand dbCmd = dbConnection.CreateCommand()) {
-				string sqlQuery = String.Format("SELECT first_name, last_name from patients where patient_id = {0}", patient_id);
+				string sqlQuery = String.Format("SELECT * from patients where patient_id = {0}", patient_id);
 				dbCmd.CommandText = sqlQuery;
 				using (IDataReader reader = dbCmd.ExecuteReader()) {
 					while (reader.Read()) {
-						string first_name = reader.GetString(0);
-						string last_name = reader.GetString(1);
-						string full_name = first_name + " " + last_name;
-						Debug.Log("GetPatientData is called");
-						this.full_name_textbox.GetComponent<Text> ().text = full_name;
-						//patientProfile.Add(new PatientProfile(first_name));
+						for (int i = 0; i < reader.FieldCount; i++) {
+							patient_data.Add(reader.GetValue(i).ToString());
+						}
+						Debug.Log("GetPatientData was called: " + patient_data[1]);
 					}
 					dbConnection.Close();
 					reader.Close();
 				}
 			}
 		}
+		return patient_data;
+	}
+
+	public void SetPatientData(int patient_id) {
+		List<string> data = GetPatientData(patient_id);
+		Debug.Log("SetPatientData was called with patient_id: " + patient_id);
+		this.id_textbox.GetComponent<Text>().text = "ID: " + data[0];
+		this.fullname_textbox.GetComponent<Text>().text = data[1] + " " + data[2];
+		this.gender_textbox.GetComponent<Text>().text = "Gender: " + data[3];
+		this.birthdate_textbox.GetComponent<Text>().text = "DoB: " + data[4];
+		this.weight_textbox.GetComponent<Text>().text = "Weight: " + data[5] + " Kg";
+		this.height_textbox.GetComponent<Text>().text = "Height: " + data[6] + " m";
+		this.phone_textbox.GetComponent<Text>().text = "Phone: " + data[7];
+		this.appointment_textbox.GetComponent<Text>().text = "Appt: " + data[8];
+		this.latest_visit_date_textbox.GetComponent<Text>().text = data[9];
+		this.latest_visit_doc_textbox.GetComponent<Text>().text = data[10];
 	}
 
 	private void InsertPatientData (string first_name, string last_name) {
