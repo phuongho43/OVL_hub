f752f3616e306dda2ce9cd58972cf3838c3c6752
phuongho43 <phuongho43@gmail.com>
2016-08-20 12:38:27 -0700

    Rest of device modules are functional (centrifuge,therm,imaging)
    Takes finished samples from extraction and stores it in an intermediate
    list. Update() is used to instantiate a centrifuge prefab + initiation
    dialog whenever there are items in the list. After samples are selected
    and confirmed, more samples can be added using the "+" button at the
    top. After the switch is locked in and progress reaches 100%, the same
    happens for Thermocycler and Imager. Next, functionality needs to be
    coded for when Imager reaches 100%. When this happens, the viral load
    reading should be recorded for each patient and the database be updated
    accordingly.

---
+++ Assets/Scripts/Device.cs
@@ -0,0 +1,165 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Device : MonoBehaviour {
+
+    public bool deviceStandby = false;
+    public GameObject patientRepPrefab;
+    public GameObject initPanelSelectionPrefab;
+    public KeyCode startTrackingKey;
+    private GameObject devicesManager;
+    private GameObject deviceTitle;
+    private GameObject initPanel;
+    private GameObject initPanelSampleName;
+    private GameObject donePanel;
+    private Transform repListScrollContent;
+    private GameObject toggleSwitch;
+    private GameObject progressCircle;
+    private GameObject percentCompletedText;
+    private int progress = 0;
+    private string currentSampleName = "";
+
+	// Use this for initialization
+	void Awake () {
+        devicesManager = GameObject.FindGameObjectWithTag("DevicesManager");
+        deviceTitle = transform.GetChild(1).gameObject;
+        initPanel = transform.GetChild(6).gameObject;
+        donePanel = transform.GetChild(7).gameObject;
+        repListScrollContent = transform.GetChild(5).GetChild(0);
+        toggleSwitch = transform.GetChild(3).gameObject;
+        progressCircle = transform.GetChild(4).GetChild(0).gameObject;
+        percentCompletedText = transform.GetChild(4).GetChild(1).GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (toggleSwitch.GetComponent<Toggle>().isOn) {
+            StartTracking(startTrackingKey);
+        }
+        else {
+            StopTracking();
+        }
+        if (progress >= 100) {
+            donePanel.SetActive(true);
+            toggleSwitch.GetComponent<Toggle>().isOn = false;
+            progress = 0;
+        }
+	}
+
+    public void SetDeviceTitle(string deviceType, string number) {
+        string newTitle = deviceType + " #" + number;
+        deviceTitle.GetComponent<Text>().text = newTitle;
+    }
+
+    public void SetFirstInitPanel(string sampleName) {
+        if (initPanel.activeSelf == false) {
+            initPanel.SetActive(true);
+            initPanelSampleName = transform.GetChild(6).GetChild(1).GetChild(1).gameObject;
+            initPanelSampleName.GetComponent<Text>().text = sampleName;
+            currentSampleName = sampleName;
+        }
+    }
+
+    public void SetLaterInitPanel(List<string> inputSamples) {
+        Transform selectedSamplesScrollContent = transform.GetChild(6).GetChild(1).GetChild(1).GetChild(0);
+        if (initPanel.activeSelf == false) {
+            initPanel.SetActive(true);
+            foreach (string sampleName in inputSamples) {
+                GameObject selectionInstance = Instantiate(initPanelSelectionPrefab) as GameObject;
+                selectionInstance.transform.SetParent(selectedSamplesScrollContent.transform, false);
+                selectionInstance.GetComponentInChildren<Text>().text = sampleName;
+            }
+        }
+    }
+
+    public void InstantiatePatientRep() {
+        GameObject patientRepInstance = Instantiate(patientRepPrefab) as GameObject;
+        patientRepInstance.transform.SetParent(repListScrollContent.transform, false);
+        patientRepInstance.GetComponent<PatientRepManager>().SetName(currentSampleName);
+    }
+
+    public void InstantiateSelectedPatientReps(int finishedSamplesList) {
+        Transform selectedSamplesScrollContent = transform.GetChild(6).GetChild(1).GetChild(1).GetChild(0);
+        foreach (Transform selection in selectedSamplesScrollContent.transform) {
+            if (selection.GetComponentInChildren<Toggle>().isOn) {
+                selection.GetComponentInChildren<Toggle>().isOn = false;
+                string sampleName = selection.GetComponentInChildren<Text>().text;
+                GameObject patientRepInstance = Instantiate(patientRepPrefab) as GameObject;
+                patientRepInstance.transform.SetParent(repListScrollContent.transform, false);
+                patientRepInstance.GetComponent<PatientRepManager>().SetName(sampleName);
+                if (finishedSamplesList == 0) {
+                    devicesManager.GetComponent<DevicesManager>().finishedExtraction.Remove(sampleName);
+                }
+                else if (finishedSamplesList == 1) {
+                    devicesManager.GetComponent<DevicesManager>().finishedCentrifugation.Remove(sampleName);
+                }
+                else if (finishedSamplesList == 2) {
+                    devicesManager.GetComponent<DevicesManager>().finishedPCR.Remove(sampleName);
+                }
+            }
+        }
+        foreach (Transform child in selectedSamplesScrollContent) {
+            GameObject.Destroy(child.gameObject);
+        }
+        deviceStandby = true;
+    }
+
+    public void SelectMoreSamples() {
+        deviceStandby = false;
+    }
+
+    public void StartTracking(KeyCode testKey) {// Place this function on the toggle switch: --> "DONE!" panel pops up when progress is at 100%
+        if (Input.GetKeyDown(testKey)) { // Change this when arduino integration finished
+            progress += 5;
+            progressCircle.GetComponent<Image>().fillAmount = progress / 100.0f;
+            percentCompletedText.GetComponent<Text>().text = progress.ToString() + "%";
+        }
+        //DONE 5. Switch ON: adjust progress circle based on potentiometer reading
+    }
+
+    public void StopTracking() {
+        progress = 0;
+        progressCircle.GetComponent<Image>().fillAmount = 0f;
+        percentCompletedText.GetComponent<Text>().text = "0%";
+        //DONE 5b. Switch OFF: resets progress circle back to standby
+    }
+
+    public void StoreFinishedSamples(int finishedSamplesList) {// Place this function on the "OK" button of the "DONE!" panel
+        foreach (Transform sample in repListScrollContent) {
+            string sampleName = sample.gameObject.GetComponent<PatientRepManager>().sampleName;
+            List<string> finishedSamples = new List<string>();
+            if (finishedSamplesList == 0) {
+                finishedSamples = devicesManager.GetComponent<DevicesManager>().finishedExtraction;
+            }
+            else if (finishedSamplesList == 1) {
+                finishedSamples = devicesManager.GetComponent<DevicesManager>().finishedCentrifugation;
+            }
+            else if (finishedSamplesList == 2) {
+                finishedSamples = devicesManager.GetComponent<DevicesManager>().finishedPCR;
+            }
+            finishedSamples.Add(sampleName);
+            GameObject.Destroy(sample.gameObject);
+        }
+        donePanel.SetActive(false);
+        if (finishedSamplesList == 0) {
+            devicesManager.GetComponent<DevicesManager>().activeExtractors.Remove(transform.gameObject);
+        }
+        else if (finishedSamplesList == 1) {
+            devicesManager.GetComponent<DevicesManager>().activeCentrifuges.Remove(transform.gameObject);
+        }
+        else if (finishedSamplesList == 2) {
+            devicesManager.GetComponent<DevicesManager>().activeThermos.Remove(transform.gameObject);
+        }
+        GameObject.Destroy(transform.gameObject);
+        //DONE 7. store all patientRepPrefab in List<> extractionFinished
+        //DONE 8. Destroy all children in extractorSamplesScrollContent
+        //DONE 8b. Destroy entire device main panel
+    }
+
+    public void RecordViralLoad() {
+        
+    }
+}
--- Assets/Scripts/DevicesManager.cs
+++ Assets/Scripts/DevicesManager.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-
+//refactor the 4 functions into 1
 public class DevicesManager : MonoBehaviour {
 
     // Labels for samples
@@ -21,7 +21,30 @@ public class DevicesManager : MonoBehaviour {
     public List<GameObject> activeCentrifuges = new List<GameObject>();
     private GameObject availableCentrifuge;
     public List<string> finishedCentrifugation = new List<string>();
+    // ThermoCycler
+    public Transform thermoListScrollContent;
+    public GameObject thermoPrefab;
+    public List<GameObject> activeThermos = new List<GameObject>();
+    private GameObject availableThermo;
+    public List<string> finishedPCR = new List<string>();
+    // Imager
+    public Transform imagerListScrollContent;
+    public GameObject imagerPrefab;
+    public List<GameObject> activeImagers = new List<GameObject>();
+    private GameObject availableImager;
 
+
+    void Update () {
+        if (finishedExtraction.Count > 0) {
+            PrepCentrifuge();
+        }
+        if (finishedCentrifugation.Count > 0) {
+            PrepThermoCycler();
+        }
+        if (finishedPCR.Count > 0) {
+            PrepImager();
+        }
+    }
 
     public void PrepFirstDevice() {
         string patientID = idTextbox.GetComponent<Text>().text.Trim();
@@ -31,7 +54,7 @@ public class DevicesManager : MonoBehaviour {
         string numberForNewExtractor = (currentNumberOfExtractors + 1).ToString();
         bool allExtractorsBusy = true;
         foreach (GameObject extractor in activeExtractors) {
-            allExtractorsBusy = extractor.transform.GetChild(2).GetComponent<Toggle>().isOn;
+            allExtractorsBusy = extractor.transform.GetChild(3).GetComponent<Toggle>().isOn;
             if (allExtractorsBusy == false) {
                 availableExtractor = extractor;
                 break;
@@ -40,12 +63,12 @@ public class DevicesManager : MonoBehaviour {
         if (currentNumberOfExtractors == 0 || allExtractorsBusy) {
             GameObject extractorInstance = Instantiate(extractorPrefab) as GameObject;
             extractorInstance.transform.SetParent(extractorListScrollContent.transform, false);
-            extractorInstance.GetComponent<FirstDevice>().SetDeviceTitle("Extractor", numberForNewExtractor);
+            extractorInstance.GetComponent<Device>().SetDeviceTitle("Extractor", numberForNewExtractor);
             activeExtractors.Add(extractorInstance);
-            extractorInstance.GetComponent<FirstDevice>().SetInitPanel(newSampleName);
+            extractorInstance.GetComponent<Device>().SetFirstInitPanel(newSampleName);
         }
         else if (!allExtractorsBusy) {
-            availableExtractor.GetComponent<FirstDevice>().SetInitPanel(newSampleName);
+            availableExtractor.GetComponent<Device>().SetFirstInitPanel(newSampleName);
         }
         //DONE 1. get patient id and last name from profile page
         //DONE 2. instantiate extractorPrefab as child of extractorListScrollContent if (zero extractors in scrollcontent || all of them are in middle of tests)
@@ -57,7 +80,27 @@ public class DevicesManager : MonoBehaviour {
         //*      ** can go back to main page to start test for other profiles or click patientRep on the samples list to remove it       
     }
 
-    public void PrepMiddleDevice(string deviceType) {// Place this function in the Update() function: use an if statement to check whether there are any samples in finishedSamples        
+    public void PrepCentrifuge() {// Place this function in the Update() function: use an if statement to check whether there are any samples in finishedSamples        
+        int currentNumberOfCentrifuges = centriListScrollContent.childCount;
+        string numberForNewCentrifuge = (currentNumberOfCentrifuges + 1).ToString();
+        bool allCentrifugesBusy = true;
+        foreach (GameObject centrifuge in activeCentrifuges) {
+            allCentrifugesBusy = centrifuge.transform.GetChild(3).GetComponent<Toggle>().isOn;
+            if (allCentrifugesBusy == false) {
+                availableCentrifuge = centrifuge;
+                break;
+            }
+        }
+        if (currentNumberOfCentrifuges == 0 || allCentrifugesBusy) {
+            GameObject centrifugeInstance = Instantiate(centrifugePrefab) as GameObject;
+            centrifugeInstance.transform.SetParent(centriListScrollContent.transform, false);
+            centrifugeInstance.GetComponent<Device>().SetDeviceTitle("Centrifuge", numberForNewCentrifuge);
+            activeCentrifuges.Add(centrifugeInstance);
+            centrifugeInstance.GetComponent<Device>().SetLaterInitPanel(finishedExtraction);
+        }
+        else if (!allCentrifugesBusy && !availableCentrifuge.GetComponent<Device>().deviceStandby) {
+            availableCentrifuge.GetComponent<Device>().SetLaterInitPanel(finishedExtraction);
+        }
         // 9. Update(): if (extractionFinished is not empty && (zero centrifuges in scrollcontent || all of them are in middle of tests))
         //      1. instantiate centriPrefab as child of centriListScrollContent
         //      2. set initCentPanel active for available centrifuge (go by index: first sample(List) goes to first device(List));
@@ -70,7 +113,51 @@ public class DevicesManager : MonoBehaviour {
         // 13. adjust progress circle based on potentiometer reading
         // 13b. Abort:resets progress circle back to standby
         // 14. if (progress = 100%) --> 
+    }
+    public void PrepThermoCycler() {
+        int currentNumberOfThermos = thermoListScrollContent.childCount;
+        string numberForNewThermo = (currentNumberOfThermos + 1).ToString();
+        bool allThermosBusy = true;
+        foreach (GameObject thermo in activeThermos) {
+            allThermosBusy = thermo.transform.GetChild(3).GetComponent<Toggle>().isOn;
+            if (allThermosBusy == false) {
+                availableThermo = thermo;
+                break;
+            }
+        }
+        if (currentNumberOfThermos == 0 || allThermosBusy) {
+            GameObject thermoInstance = Instantiate(thermoPrefab) as GameObject;
+            thermoInstance.transform.SetParent(thermoListScrollContent.transform, false);
+            thermoInstance.GetComponent<Device>().SetDeviceTitle("Thermocyler", numberForNewThermo);
+            activeThermos.Add(thermoInstance);
+            thermoInstance.GetComponent<Device>().SetLaterInitPanel(finishedCentrifugation);
+        }
+        else if (!allThermosBusy && !availableThermo.GetComponent<Device>().deviceStandby) {
+            availableThermo.GetComponent<Device>().SetLaterInitPanel(finishedCentrifugation);
+        }
         // 15. Do same for thermocycler and imager
+    }
+    public void PrepImager() {
+        int currentNumberOfImagers = imagerListScrollContent.childCount;
+        string numberForNewImager = (currentNumberOfImagers + 1).ToString();
+        bool allImagersBusy = true;
+        foreach (GameObject imager in activeImagers) {
+            allImagersBusy = imager.transform.GetChild(3).GetComponent<Toggle>().isOn;
+            if (allImagersBusy == false) {
+                availableImager = imager;
+                break;
+            }
+        }
+        if (currentNumberOfImagers == 0 || allImagersBusy) {
+            GameObject imagerInstance = Instantiate(imagerPrefab) as GameObject;
+            imagerInstance.transform.SetParent(imagerListScrollContent.transform, false);
+            imagerInstance.GetComponent<Device>().SetDeviceTitle("Imager", numberForNewImager);
+            activeImagers.Add(imagerInstance);
+            imagerInstance.GetComponent<Device>().SetLaterInitPanel(finishedPCR);
+        }
+        else if (!allImagersBusy && !availableImager.GetComponent<Device>().deviceStandby) {
+            availableImager.GetComponent<Device>().SetLaterInitPanel(finishedPCR);
+        }
         // 16. After 100% for imager -->
         // 17. get hiv load reading for corresponding samples
         // 18. INSERT hiv load where patient_id = xxx for each sample in imager
